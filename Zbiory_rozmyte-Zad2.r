361a450e3d191979cf34b5eb867507adbf92bf96
Michal_Parchanowicz <michal.parchanowicz@gmail.com>
2023-12-18 01:39:44 +0100

    develop

--- Zbiory_rozmyte-Zad2.r
+++ Zbiory_rozmyte-Zad2.r
@@ -2,7 +2,7 @@
 library(sets)
 
 # Wczytaj dane z pliku CSV do tablicy decyzyjnej
-wine_data <- read.table("C:\\Users\\Michał\\OneDrive\\Pulpit\\R\\wine.txt", header = TRUE, sep = " ")
+wine_data <- read.table("./wine.txt", header = TRUE, sep = " ")
 
 # Wyświetl strukturę danych
 print("wine_data:")
@@ -22,21 +22,21 @@ str(test_data)
 # Zbiór rozmyty dla kolumny "alcohol"
 u1 = seq(11, 15, 0.01)
 low_alcohol  = fuzzy_cone_gset(center=11, radius=1.5, universe=u1)
-avg_alcohol = fuzzy_trapezoid_gset(corners = c(12, 12.5, 13.5, 14), universe=u1)
+avg_alcohol = fuzzy_trapezoid_gset(corners=tuple(12, 12.5, 13.5, 14), universe=u1)
 high_alcohol = fuzzy_cone_gset(center=15, radius=1.5, universe=u1)
 
 alcohol = fuzzy_variable(low_alcohol =low_alcohol, avg_alcohol=avg_alcohol, high_alcohol=high_alcohol)
-alcohol_sets = list(low_alcohol=low_alcohol, avg_alcohol=avg_alcohol, high_alcohol=high_alcohol)
+alcohol_sets = set(low_alcohol=low_alcohol, avg_alcohol=avg_alcohol, high_alcohol=high_alcohol)
 plot(alcohol)
 
 
 # Zbiór rozmyty dla kolumny "malic_acid"
-u2 <- seq(0, 6, 0.01)
-low_malic_acid  <- fuzzy_cone_gset(center=0, radius=3, universe=u2)
-high_malic_acid <- fuzzy_cone_gset(center=6, radius=4, universe=u2)
+u2 = seq(0, 6, 0.01)
+low_malic_acid  = fuzzy_cone_gset(center=0, radius=3, universe=u2)
+high_malic_acid = fuzzy_cone_gset(center=6, radius=4, universe=u2)
 
-malic_acid <- fuzzy_variable(low_malic_acid =low_malic_acid, high_malic_acid=high_malic_acid)
-malic_acid_sets <- list(low_malic_acid=low_malic_acid, high_malic_acid=high_malic_acid)
+malic_acid = fuzzy_variable(low_malic_acid=low_malic_acid, high_malic_acid=high_malic_acid)
+malic_acid_sets = set(low_malic_acid=low_malic_acid, high_malic_acid=high_malic_acid)
 plot(malic_acid)
 
 
@@ -46,7 +46,7 @@ low_color_intensity  = fuzzy_cone_gset(center=0, radius=8, universe=u3)
 high_color_intensity = fuzzy_cone_gset(center=12, radius=8, universe=u3)
 
 color_intensity = fuzzy_variable(low_color_intensity =low_color_intensity, high_color_intensity=high_color_intensity)
-color_intensity_sets = list(low_color_intensity =low_color_intensity, high_color_intensity=high_color_intensity)
+color_intensity_sets = set(low_color_intensity =low_color_intensity, high_color_intensity=high_color_intensity)
 plot(color_intensity)
 
 
@@ -57,9 +57,9 @@ avg_cultivar = fuzzy_triangular_gset(corners = c(1, 2, 3), universe=u4)
 high_cultivar = fuzzy_cone_gset(center=3, radius=1, universe=u4)
 
 cultivar = fuzzy_variable(low_cultivar=low_cultivar, avg_cultivar=avg_cultivar, high_cultivar=high_cultivar)
-cultivar_sets = list(low_cultivar=low_cultivar, avg_cultivar=avg_cultivar, high_cultivar=high_cultivar)
+cultivar_sets = set(low_cultivar=low_cultivar, avg_cultivar=avg_cultivar, high_cultivar=high_cultivar)
 plot(cultivar)
-
+plot(alcohol)
 
 # Funkcja do zamiany wartości liczbowych na symboliczne na podstawie zbiorów rozmytych
 fuzzy_to_symbolic <- function(value, breaks, labels) {
@@ -67,8 +67,13 @@ fuzzy_to_symbolic <- function(value, breaks, labels) {
   return(as.character(cut_value))
 }
 
+alcohol_fuzzy_to_symbolic <- function(values) {
+  cut_value <- cut(value, breaks = breaks, labels = labels, include.lowest = TRUE)
+  return(as.character(cut_value))
+}
+
 # Definicja zbiorów rozmytych
-u1 <- seq(11, 15, 0.5)
+
 breaks_alcohol <- c(-Inf, 12, 14, Inf)
 labels_alcohol <- c("low_alcohol", "avg_alcohol", "high_alcohol")
 
@@ -97,15 +102,7 @@ train_data$color_intensity <- fuzzy_to_symbolic(train_data$color_intensity, brea
 train_data$cultivar <- fuzzy_to_symbolic(train_data$cultivar, breaks_cultivar, labels_cultivar)
 
 # Wyświetl przekształcone dane
-print(train_data)
+#print(train_data)
 
-low_malic_acid <- malic_acid_sets[["low_malic_acid"]]
-
-# Find the index of value malic_acid=2
-low_malic_acid_idx <- which(low_malic_acid$values == 2)
-
-# Retrieve the membership value
-low_malic_acid_membership <- low_malic_acid$membership
-
-# Display the membership value for malic_acid=2
-print(low_malic_acid_membership[low_malic_acid_idx])
+#print(color_intensity_sets$low_color_intensity)
+print(attributes(alcohol_sets$avg_alcohol[12])[[2]])
